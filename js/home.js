c7d38250d0abaa873944523b208b2440da861d6d
harper777 <livethelife202012@gmail.com>
2026-04-24 16:04:28 +0800

    增加懸浮按鈕

--- js/home.js
+++ js/home.js
@@ -24,3 +24,14 @@ function initHomeLocationMap() {
 document.addEventListener("DOMContentLoaded", function () {
   initHomeLocationMap();
 });
+
+
+if ("scrollRestoration" in history) {
+  history.scrollRestoration = "manual";
+}
+
+window.addEventListener("load", function () {
+  setTimeout(function () {
+    window.scrollTo(0, 0);
+  }, 0);
+});
